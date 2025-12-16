cdc263a3d8ef8dc7e7f122d4d074caa2276e4bf0
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-12-15 23:56:28 -0300

    changed page title, chart position, sliders standard values and added 2 conditions related to the remaining points value

--- recommendation_system_dashboard.py
+++ recommendation_system_dashboard.py
@@ -7,48 +7,80 @@ df_flavour_wheel = pd.read_csv('flavour_wheel_long.csv')
 
 st.set_page_config(layout='wide')
 
+st.title('DramMatch - Your whisky recommender')
+
 with st.sidebar:
 
-    fruity_level = st.slider('Fruity', min_value=0, max_value=100, value=9)
+    st.header('Adjust the sliders to reflect your flavour preferences')
 
-    floral_level = st.slider('Floral', min_value=0, max_value=100, value=9)
+    if 'points_remaining' not in st.session_state:
+        st.session_state.points_remaining = 100
+    
+    points_text = st.text(f'Preference points remaining: {st.session_state.points_remaining}/100')
 
-    spicy_level = st.slider('Spicy', min_value=0, max_value=100, value=9)
+    fruity_level = st.slider('Fruity', min_value=0, max_value=100, value=0)
 
-    woody_level = st.slider('Woody', min_value=0, max_value=100, value=9)
+    floral_level = st.slider('Floral', min_value=0, max_value=100, value=0)
 
-    cereal_level = st.slider('Cereal', min_value=0, max_value=100, value=8)
+    spicy_level = st.slider('Spicy', min_value=0, max_value=100, value=0)
 
-    peaty_level = st.slider('Peaty', min_value=0, max_value=100, value=8)
+    woody_level = st.slider('Woody', min_value=0, max_value=100, value=0)
 
-    feinty_level = st.slider('Feinty', min_value=0, max_value=100, value=8)
+    cereal_level = st.slider('Cereal', min_value=0, max_value=100, value=0)
 
-    sulphury_level = st.slider('Sulphury', min_value=0, max_value=100, value=8)
+    peaty_level = st.slider('Peaty', min_value=0, max_value=100, value=0)
 
-    sweet_level = st.slider('Sweet', min_value=0, max_value=100, value=8)
+    feinty_level = st.slider('Feinty', min_value=0, max_value=100, value=0)
 
-    nutty_level = st.slider('Nutty', min_value=0, max_value=100, value=8)
+    sulphury_level = st.slider('Sulphury', min_value=0, max_value=100, value=0)
 
-    coastal_level = st.slider('Coastal', min_value=0, max_value=100, value=8)
+    sweet_level = st.slider('Sweet', min_value=0, max_value=100, value=0)
 
-    roasted_level = st.slider('Roasted', min_value=0, max_value=100, value=8)
+    nutty_level = st.slider('Nutty', min_value=0, max_value=100, value=0)
+
+    coastal_level = st.slider('Coastal', min_value=0, max_value=100, value=0)
+
+    roasted_level = st.slider('Roasted', min_value=0, max_value=100, value=0)
 
 user_input_vector = [
     fruity_level, floral_level, spicy_level, woody_level, cereal_level, peaty_level,
     feinty_level, sulphury_level, sweet_level, nutty_level, coastal_level, roasted_level
 ]
 
-user_input_vector = [x / 100 for x in user_input_vector]
+sum_user_input_vector = sum(user_input_vector)
+points_remaining = 100 - sum_user_input_vector
 
-recommended_bottle = make_recommendation(user_input_vector)
+st.session_state.points_remaining = points_remaining
 
-df_recommended_bottle = df_flavour_wheel[df_flavour_wheel['review_title'] == recommended_bottle]
+if st.session_state.points_remaining == 100:
+    st.space('small')
+    st.info('Please allocate your preference points using the sliders in the sidebar to get a recommendation.')
 
-print(df_recommended_bottle)
+elif st.session_state.points_remaining < 0:
+    st.space('small')
+    st.warning('You have exceeded the 100 preference points limit. Please adjust your sliders accordingly.')
 
-polar_fig = px.line_polar(df_recommended_bottle, r="perc_tasting_notes_group", theta="tasting_notes_group",
-                          line_close=True,
-                          template="plotly_white",
-                          title=recommended_bottle)
+else:
 
-st.plotly_chart(polar_fig, width='stretch', config={'displayModeBar': False})
+    points_text.text(f'Preference points remaining: {points_remaining}/100')
+
+    user_input_vector = [x / 100 for x in user_input_vector]
+
+    recommended_bottle = make_recommendation(user_input_vector)
+
+    df_recommended_bottle = df_flavour_wheel[df_flavour_wheel['review_title'] == recommended_bottle]
+
+    polar_fig = px.line_polar(df_recommended_bottle, r="perc_tasting_notes_group", theta="tasting_notes_group",
+                            line_close=True,
+                            template="plotly_white",
+                            title=None)
+
+    st.space('small')
+
+    st.subheader(recommended_bottle)
+
+    col_chart, col_fake = st.columns([2, 3])
+
+    with col_chart:
+        
+        st.plotly_chart(polar_fig, width='stretch', config={'displayModeBar': False})
